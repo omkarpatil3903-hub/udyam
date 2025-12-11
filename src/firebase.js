03e8ec7d4bc47993c210b3f9e4fd801dbe08b73f
omkar patil <omkarpatil3903@gmail.com>
2025-12-11 14:53:49 +0530

    udpage

--- src/firebase.js
+++ src/firebase.js
@@ -10,14 +10,19 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+   apiKey: "AIzaSyDyhO59791kCGBaIprkoeCUWbySpjj1rXs",
+  authDomain: "udyam-837b2.firebaseapp.com",
+  projectId: "udyam-837b2",
+  storageBucket: "udyam-837b2.firebasestorage.app",
+  messagingSenderId: "1006471527250",
+  appId: "1:1006471527250:web:6b5393a44ed31c8981988b" 
+  // apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  // authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  // projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  // measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
